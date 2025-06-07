e8f9d65c7ea33fc4107eda622ca0eea551b87b53
deliciousdabbe <deliciousdabble@gmail.com>
2025-06-07 19:59:28 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// Script for floating order button
+document.getElementById("order-now").addEventListener("click", function() {
+    alert("Thank you for your interest in Apple Airpods Pro 2nd Gen! We'll contact you shortly.");
+});
