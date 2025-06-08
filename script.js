af0d1f74f971cdc0bd7b43887831463072dcae52
deliciousdabbe <deliciousdabble@gmail.com>
2025-06-08 14:27:22 +0600

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-// Script for floating order button
-document.getElementById("order-now").addEventListener("click", function() {
-    alert("Thank you for your interest in Apple Airpods Pro 2nd Gen! We'll contact you shortly.");
-});
